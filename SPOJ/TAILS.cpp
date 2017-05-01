bb790603d5b41c828b7023ce72ff1489b8f9d307
engyebrahim <engyebrahim1868@gmail.com>
2017-05-01 10:30:56 +0200

    Update TAILS.cpp

--- SPOJ/TAILS.cpp
+++ SPOJ/TAILS.cpp
@@ -1,19 +1,22 @@
+/*
+make dp+bits
+for every index try change it or not 
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int dp[2000000][21];
 int fun(bitset<20>b,int i)
 {
-     //cout<<b.to_string()<<" "<<i<<endl;
      bitset<20> tmp;
      int a=b.to_ulong();
      if(a==0) return 0;
      if(i==20) return 1e+9;
      if(dp[a][i]!=-1) return dp[a][i];
      dp[a][i]=fun(b,i+1);
-          tmp=b;
-          if(!i) tmp[i]=!b[i],tmp[i+1]=!b[i+1];
-          else if(i==19) tmp[i]=!b[i],tmp[i-1]=!b[i-1];
-          else tmp[i]=!b[i],tmp[i+1]=!b[i+1],tmp[i-1]=!b[i-1];
+     tmp=b;
+     if(!i) tmp[i]=!b[i],tmp[i+1]=!b[i+1];
+     else if(i==19) tmp[i]=!b[i],tmp[i-1]=!b[i-1];
+     else tmp[i]=!b[i],tmp[i+1]=!b[i+1],tmp[i-1]=!b[i-1];
      dp[a][i]=min(dp[a][i],1+fun(tmp,i+1));
      return dp[a][i];
 }
